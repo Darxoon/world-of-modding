28239497496775023aeb20024066115c97591d42
Darxoon <Jakob.priemer@gmail.com>
2019-03-23 12:15:59 +0100

    finished raycasting and moved it into another script to test Purple Scientist's solution

---
+++ Assets/_Scripts/Attach_1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attach_1 : MonoBehaviour
+{
+    private Drag drag;
+
+    private void Start()
+    {
+        drag = GetComponent<Drag>();
+    }
+
+    public void Attach(int rays, string ballLayerMask)
+    {
+        for (int i = 0; i < rays; i++)
+        {
+            // the vector for the ray
+            Vector3 euler = new Vector3(i / (rays * 1f) * 360f, 90f, 0f);
+            // show the ray
+            Debug.DrawRay(transform.position, (Quaternion.Euler(euler) * Vector3.forward) * 50, Color.blue);
+            // cast the ray
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Quaternion.Euler(euler) * Vector3.forward, 50, LayerMask.GetMask(ballLayerMask));
+            if (hit)
+            {
+                // if it hit something
+                Debug.DrawLine(transform.position, hit.point, Color.green);
+            }
+        }
+
+    }
+}
--- Assets/_Scripts/Drag.cs
+++ Assets/_Scripts/Drag.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Drag : MonoBehaviour
 {
-    [SerializeField] private string ballLayerMask;
+    [SerializeField] private string ballLayerMask = "Attached Balls";
+    [SerializeField] private int rays = 50;
+
+    [Header("Debugging")]
 
     [SerializeField] private bool isDragged = false;
-    [SerializeField] private Rigidbody2D rigid;
     [SerializeField] private GameObject drag;
 
+    private Attach_1 attach_1;
+    private Rigidbody2D rigid;
+
     //public Vector3 euler = new Vector3(90f, 0f, 1f);
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        attach_1 = GetComponent<Attach_1>();
+    }
 
     private void Update()
     {
@@ -38,50 +49,33 @@ public class Drag : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(isDragged||true)
+        if(isDragged)
         {
-            Debug.Log("what's up=");
             // positioning
-            //gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
-            //Vector3 mousePosition = Input.mousePosition;
-            //Vector2 point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 3600f));
-            //transform.position = new Vector3(point.x, point.y, 0);
+            gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            Vector3 mousePosition = Input.mousePosition;
+            Vector2 point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 3600f));
+            transform.position = new Vector3(point.x, point.y, 0);
 
-            //// changing the layer
-            //gameObject.layer = LayerMask.NameToLayer("Selected Ball");
+            // changing the layer
+            gameObject.layer = LayerMask.NameToLayer("Selected Ball");
 
             // attaching 
-            for (int i = 0; i < 10; i++)
-            {
-                Debug.Log("new iteration");
-                Vector3 euler = new Vector3(90f, 0f, i / 10f * 360f);
-                Debug.Log(euler);
-                // show where the ray started
-                Debug.DrawLine(transform.position, transform.position + new Vector3(0f, 10f, 0f));
-                // show the ray
-                Debug.Log(Quaternion.Euler(euler) * Vector3.forward);
-                Debug.DrawRay(transform.position, (Quaternion.Euler(euler) * Vector3.forward) * 50, Color.blue);
-                // cast the ray
-                RaycastHit2D hit = Physics2D.Raycast(transform.position, Quaternion.Euler(new Vector3(0f, 0f, i / 10) * 360) * Vector3.forward, 50, LayerMask.GetMask(ballLayerMask));
-                if (hit)
-                {
-                    Debug.DrawLine(hit.point, Camera.main.transform.position, Color.green);
-                    Debug.Log(hit);
-                }
-            }
+            attach_1.Attach(rays, ballLayerMask);
+
 
             // stop dragging
-            //if (Input.GetMouseButtonUp(0))
-            //{
-            //    if (drag != null) { drag.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None; }
-            //    isDragged = false;
-            //    drag = null;
-            //    gameObject.layer = LayerMask.NameToLayer("Detached Balls");
-            //    Debug.Log("stopped dragging");
-            //}
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (drag != null) { drag.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None; }
+                isDragged = false;
+                drag = null;
+                gameObject.layer = LayerMask.NameToLayer("Detached Balls");
+                Debug.Log("stopped dragging");
+            }
 
         }
-        
+
 
     }
 
