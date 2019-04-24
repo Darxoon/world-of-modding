7e882c43ee11694c5160c240f86011e607777c10
Darxoon <Jakob.priemer@gmail.com>
2019-04-24 12:42:43 +0200

    added Button Scripts

---
+++ Assets/_Scripts/ButtonController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    [SerializeField] ButtonHitbox hitbox;
+    [SerializeField] Vector3 normalScale;
+    [SerializeField] float hoveringMultiplicator;
+    [SerializeField] float interpolationCoefficient;
+
+    private void Update()
+    {
+        if (hitbox.hovering)
+            transform.localScale = Vector3.Lerp(transform.localScale, normalScale * hoveringMultiplicator, interpolationCoefficient);
+        else
+            transform.localScale = Vector3.Lerp(transform.localScale, normalScale, interpolationCoefficient);
+    }
+}
