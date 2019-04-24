6f94f01fe00e0335b9a727d7e612da44385b10e8
Purple Scientist <yanikyanus@gmail.com>
2019-04-24 09:38:50 +0300

    Fixed the main menu

---
+++ Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public bool isStart;
+public bool isOptions;
+public bool isQuit;
+
+void OnMouseUp(){
+	if(isStart)
+	{
+		Application.LoadLevel(1);
+	}
+	if (isQuit)
+	{
+		Application.Quit();
+	}
+} 
---
+++ Assets/_Scripts/MouseHover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+void Start(){
+	renderer.material.color = Color.white;
+}
+
+void OnMouseEnter(){
+	renderer.material.color = Color.black;
+}
+
+void OnMouseExit() {
+	renderer.material.color = Color.white;
+}
