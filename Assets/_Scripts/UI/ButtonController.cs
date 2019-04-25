6534166683642a8509fe7a9d4bca1bd7cc45c393
Darxoon <Jakob.priemer@gmail.com>
2019-04-25 10:11:55 +0200

    continued

--- Assets/_Scripts/UI/ButtonController.cs
+++ Assets/_Scripts/UI/ButtonController.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour
 {
-    [SerializeField] ButtonHitbox hitbox;
     [SerializeField] Vector3 normalScale;
     [SerializeField] float hoveringMultiplicator;
     [SerializeField] float interpolationCoefficient;
 
-    private void Update()
+    //private void Update()
+    //{
+    //    transform.localScale = Vector3.Lerp(transform.localScale, normalScale * hoveringMultiplicator, interpolationCoefficient);
+    //    transform.localScale = Vector3.Lerp(transform.localScale, normalScale, interpolationCoefficient);
+    //}
+
+    private void OnGUI()
     {
-        if (hitbox.hovering)
-            transform.localScale = Vector3.Lerp(transform.localScale, normalScale * hoveringMultiplicator, interpolationCoefficient);
-        else
-            transform.localScale = Vector3.Lerp(transform.localScale, normalScale, interpolationCoefficient);
+        //Rect buttonRect = new Rect(button.x + window.x, button.y + window.y + 1, button.width, button.height); 
+
+        //if (buttonRect.Contains(Event.current.mousePosition))
+        //{
+        //}
     }
 }
